d2b01949eda23f29bb0afa78a178480c71a2a95b
Mayank <legendmayank1628@gmail.com>
2024-06-02 02:56:30 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,17 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.get("/",(req,res)=>{
+    res.json({"success":"servers started succesfully"})
+})
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error('Failed to connect to MongoDB', err));
 
 // Import Routes
-const authRoutes = require('./routes/auth');
-app.use('/', authRoutes);
+// const authRoutes = require('./routes/auth');
+// app.use('/', authRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
